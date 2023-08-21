dd8ae598fd148af4536d06806d79c80cd0a142e5
ddrelk <drelk53@yahoo.com>
2023-08-21 12:09:00 +0100

    Helper Files Added

---
+++ api_extractor.py
@@ -0,0 +1,32 @@
+import json
+import requests
+from colorama import Fore, init
+init()
+
+API_KEY = "13043d11"
+URL = f'http://www.omdbapi.com/?i=tt3896198&apikey={API_KEY}'
+
+
+def data_extractor(movie):
+    try:
+        response = requests.get(f'http://www.omdbapi.com/?i=tt3896198&apikey={API_KEY}&t={movie}')
+        response.raise_for_status()  # raise an exception if the response status code is not 200 OK
+        data_result = response.json()
+        return data_result
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED + "An error occurred while trying to access the API:", e, Fore.RESET)
+        return None
+
+
+def get_imdb_link(title):
+    # Make a request to the OMDb API
+    response = requests.get(f'http://www.omdbapi.com/?apikey={API_KEY}&t={title}')
+
+    # Parse the JSON response
+    data = json.loads(response.text)
+
+    # Extract the IMDb link
+    imdb_id = data.get('imdbID')
+    imdb_link = f'https://www.imdb.com/title/{imdb_id}/'
+
+    return imdb_link
